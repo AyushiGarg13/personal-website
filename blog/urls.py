b23d1836a9c2b7951e3e91684d0f09c7f49704c8
Garg <ayushigarg67@gmail.com>
2023-06-11 16:52:51 +0530

    Added basic setting, made the pages dynamic

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,6 @@ from blog.views import index, post_details, posts
 
 urlpatterns = [
     path("", index, name="index"),
-    path("posts", posts, name="post"),
-    path("posts/<slug:post_name>", post_details, name="post_details"),
+    path("posts", posts, name="posts"),
+    path("posts/<slug:slug>", post_details, name="post_details"),
 ]
