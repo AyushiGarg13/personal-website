c3c39be0331e079537ff555829170fe7f3d378a1
Garg <ayushigarg67@gmail.com>
2023-06-27 17:16:23 +0530

    Made connections in index page with database

--- blog/admin.py
+++ blog/admin.py
@@ -4,6 +4,13 @@ from blog.models import Author, Post, Tag
 
 # Register your models here.
 
-admin.site.register(Post)
+
+class PostAdmin(admin.ModelAdmin):
+    list_filter = ("author", "tags", "date")
+    list_display = ("title", "date", "author")
+    prepopulated_fields = {"slug": ("title",)}
+
+
+admin.site.register(Post, PostAdmin)
 admin.site.register(Author)
 admin.site.register(Tag)
