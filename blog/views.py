4349d2b44df2aeefd4cf1cbd9a0a474b474ac30e
Garg <ayushigarg67@gmail.com>
2023-08-24 10:49:43 +0530

    Adding Comments

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
-from typing import Dict, Any
-
-from django.views.generic import ListView, DetailView
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from django.urls import reverse
+from django.views import View
+from django.views.generic import ListView
 
 from blog.forms import CommentForm
 from blog.models import Post
@@ -28,12 +30,28 @@ class PostsView(ListView):
     context_object_name = "all_posts"
 
 
-class PostDetailView(DetailView):
-    template_name = 'blog/post-details.html'
-    model = Post
+class PostDetailView(View):
+    def get(self, request, slug):
+        post = Post.objects.get(slug=slug)
+        context = {
+            "post": post,
+            "post_tags": post.tags.all(),
+            "comment_form": CommentForm()
+        }
+        return render(request, 'blog/post-details.html', context)
 
-    def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
-        context = super().get_context_data(**kwargs)
-        context["post_tags"] = self.object.tags.all()
-        context["comment_form"] = CommentForm()
-        return context
+    def post(self, request, slug):
+        comment_form = CommentForm(request.POST)
+        post = Post.objects.get(slug=slug)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return HttpResponseRedirect(reverse("post_details", args=[slug]))
+
+        context = {
+            "post": post,
+            "post_tags": post.tags.all(),
+            "comment_form": comment_form
+        }
+        return render(request, 'blog/post-details.html', context)
