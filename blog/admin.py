ffe5898188fecfad4330fe622c31834af111fe76
Garg <ayushigarg67@gmail.com>
2023-08-24 19:10:57 +0530

    Adding Comments

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from blog.models import Author, Post, Tag
+from blog.models import Author, Post, Tag, Comment
+
 
 # Register your models here.
 
@@ -11,6 +12,11 @@ class PostAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
 
 
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("user_name", "post")
+
+
 admin.site.register(Post, PostAdmin)
 admin.site.register(Author)
 admin.site.register(Tag)
+admin.site.register(Comment, CommentAdmin)
