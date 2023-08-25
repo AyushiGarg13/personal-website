dff3eb66a20a71c79f77e82a855fcfa57785b373
Garg <ayushigarg67@gmail.com>
2023-08-25 19:46:43 +0530

    Deployment Readiness

--- personal_website/settings.py
+++ personal_website/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from os import getenv
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +23,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# 'django-insecure-9fi1%w@6fikmlo#_0=3xp4s#bon(9(g(e#w*@k=h3_xa(w%8)8'
 SECRET_KEY = getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
